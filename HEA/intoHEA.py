e5e2c8e1cde21606262130186fc6179face51cef
Sez26 <serena.farrelly@gmail.com>
2024-06-19 10:43:06 +0800

    Planning and steps 1 and 2 of HEA building code

--- HEA/intoHEA.py
+++ HEA/intoHEA.py
@@ -14,5 +14,32 @@ b2NiTi = vasp.read_vasp(PoscarFile) # vasp.read_vasp function reads POSCAR files
 # view(b2NiTi) # Success
 
 # trying atoms functions
-b2NiTi_atnum = b2NiTi.get_atomic_numbers()
-print(b2NiTi_atnum) # Success
+# b2NiTi_atnum = b2NiTi.get_atomic_numbers()
+# print(b2NiTi_atnum) # Success
+
+# PLAN
+# 1) Split coordinates into sublattice A and B
+# 2) Assign tags to the atoms in each sublattice
+# 3) write def functions in order to change element assignment
+# 4) randomly allocate distribution from each sublattice to be reassigned
+
+# 1) Split coordinates into sublattice A and B
+# d = 3.128 A
+# offset = 1.564 A
+b2NiTi_pos = b2NiTi.get_positions()
+# print(type(b2NiTi_pos)) # np array
+# print(b2NiTi_pos.shape) # 1 row per atom, 3 columns of x,y,z
+
+# logical arrays
+b2NiTi_posA = (b2NiTi_pos[:,1] % 3.128)==0 # dependent on symmetrical square lattice (b2 specific so pretty dodge)
+# b2NiTi_posB = (b2NiTi_pos % 3.128)==1.564
+
+# 2) Assigning tags
+# sublattice A atoms are tagged with 1, sublattice B tagged 0
+subLatTags = b2NiTi_posA * 1
+# print(subLatTags.shape)
+# print(b2NiTi.get_tags().shape)
+b2NiTi.set_tags(subLatTags)
+
+# 3) Writing element assignment def functions
+ 
