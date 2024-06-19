b74532c4b4395791fa358b55eef6865cbc14488d
Sez26 <serena.farrelly@gmail.com>
2024-06-19 17:46:40 +0800

    Struggling to make the other models and noticing that the distribution of elements might not be right!

--- HEA/intoHEA_1.py
+++ HEA/intoHEA_1.py
@@ -23,20 +23,16 @@ PoscarFile = '/home/sez26/TIGP-IIP/HEA/smallPos'
 b2NiTi = vasp.read_vasp(PoscarFile) # vasp.read_vasp function reads POSCAR files as Atoms type
 
 # 1) Split coordinates into sublattice A and B
-# d = 3.128 A
-# offset = 1.564 A
-b2NiTi_pos = b2NiTi.get_positions()
+b2NiTi_atNum = b2NiTi.get_atomic_numbers()
 
 # logical arrays
-b2NiTi_A = (b2NiTi_pos % 3.128) == 0 # dependent on symmetrical square lattice (b2 specific so pretty dodge)
-b2NiTi_B = (b2NiTi_pos % 3.128)==1.564 # not necessarily needed because only 2 sublattices
-
-# find row indexes of sublattices
-b2NiTi_A = np.all(b2NiTi_A, axis = 1) # testing that spacing interval is true for all x,y and z directions + combination into column vector
-b2NiTi_B = np.all(b2NiTi_B, axis = 1)
+b2NiTi_A = b2NiTi_atNum == 28
+b2NiTi_B = b2NiTi_atNum == 22 # not necessarily needed because only 2 sublattices
 
 b2NiTi_Aidx = np.where(b2NiTi_A == True)[0] # finding the indicies, [0] to change from tuple type O/P into np.array
 b2NiTi_Bidx = np.where(b2NiTi_B == True)[0]
+
+print('length of sublattice index arrays: ', len(b2NiTi_Aidx))
 
 # 2) Assigning tags
 # sublattice A atoms are tagged with 1, sublattice B tagged 0
@@ -68,27 +64,56 @@ HEA_ChemSym = ['Co', 'Ni', 'Hf', 'Ti', 'Zr']
 # therefore additional elements only need be added.
 
 # finding total number of atoms in each lattice
-numA = len(b2NiTi_Aidx)
-numB = len(b2NiTi_Bidx)
-
+numA = sum(b2NiTi_A)
+print('total atoms:', len(b2NiTi_atNum))
+print('numA:',numA)
+numB = sum(b2NiTi_B)
+print('numB: ',numB)
 # for sublattice A, Co and Ni are in equal proportion
 numSel = int(numA/2)
+print('num Co/Ni selected: ', numSel)
 SelAidx = np.random.choice(b2NiTi_Aidx, size = numSel, replace=False)
 
 # Call replacement function
 SubLatA_HEA = ChangeElement(b2NiTi, SelAidx, HEA_ChemSym[0])
 # view(SubLatA_HEA)
 
+CoIdx = np.where(SubLatA_HEA.get_atomic_numbers() == HEA_AtNum[0])[0]
+NiIdx = np.where(SubLatA_HEA.get_atomic_numbers() == HEA_AtNum[1])[0]
+print('PreSubLat2 num Co: ', len(CoIdx))
+print('PreSubLat2 num Ni: ', len(NiIdx))
+
 # Similar for sublattice B
 # for sublattice B, Hf, Ti, and Zr are in equal proportion
 numSel = int(numB/3)
+print('num Hf/Ti/Zr selected: ', numSel)
 SelBidx = np.random.choice(b2NiTi_Bidx, size = (2,numSel), replace=False)
+print(len(SelBidx[0,:]))
 
 # Call replacement function
 SubLatBHf_HEA = ChangeElement(SubLatA_HEA, SelBidx[0,:], HEA_ChemSym[2])
 SubLatBZr_HEA = ChangeElement(SubLatBHf_HEA, SelBidx[1,:], HEA_ChemSym[4])
 
+CoIdx = np.where(SubLatBZr_HEA.get_atomic_numbers() == HEA_AtNum[0])[0]
+NiIdx = np.where(SubLatBZr_HEA.get_atomic_numbers() == HEA_AtNum[1])[0]
+print('PostSubLat2 num Co: ', len(CoIdx))
+print('PostSubLat2 num Ni: ', len(NiIdx))
+HfIdx = np.where(SubLatBZr_HEA.get_atomic_numbers() == HEA_AtNum[2])[0]
+TiIdx = np.where(SubLatBZr_HEA.get_atomic_numbers() == HEA_AtNum[3])[0]
+ZrIdx = np.where(SubLatBZr_HEA.get_atomic_numbers() == HEA_AtNum[4])[0]
+print('Postnum Hf: ', len(HfIdx))
+print('Postnum Ti: ', len(TiIdx))
+print('Postnum Zr: ', len(ZrIdx))
+
+# THIS IS RIGHT!!!
+# What changes between here and the end of my code????
+
 HEA_ordered = SubLatBZr_HEA
+
+CoIdx = np.where(HEA_ordered.get_atomic_numbers() == HEA_AtNum[0])[0]
+NiIdx = np.where(HEA_ordered.get_atomic_numbers() == HEA_AtNum[1])[0]
+print('PostSubLat2 num Co: ', len(CoIdx))
+print('PostSubLat2 num Ni: ', len(NiIdx))
 
 # view(HEA_ordered)
 
@@ -118,4 +143,18 @@ for i in [0,1,2]:
     HEA_Hf_Ti_Zr = HEA_Hf_Ti_Zr_new
 
 HEA_disordered = HEA_Hf_Ti_Zr
-view(HEA_disordered)
+# view(HEA_disordered)
+
+# 6) Swap Zr atoms
+
+# find indices of Zr atoms (list comprehension)
+CoIdx = np.where(HEA_ordered.get_atomic_numbers() == HEA_AtNum[0])[0]
+NiIdx = np.where(HEA_ordered.get_atomic_numbers() == HEA_AtNum[1])[0]
+HfIdx = np.where(HEA_ordered.get_atomic_numbers() == HEA_AtNum[2])[0]
+TiIdx = np.where(HEA_ordered.get_atomic_numbers() == HEA_AtNum[3])[0]
+ZrIdx = np.where(HEA_ordered.get_atomic_numbers() == HEA_AtNum[4])[0]
+print('num Co: ', len(CoIdx))
+print('num Ni: ', len(NiIdx))
+print('num Hf: ', len(HfIdx))
+print('num Ti: ', len(TiIdx))
+print('num Zr: ', len(ZrIdx)) # I don't understand how this is bigger than 576
