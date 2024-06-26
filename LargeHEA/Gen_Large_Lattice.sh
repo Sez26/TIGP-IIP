afd6680554bd97937b0a3400b1828233987988c6
Sez26 <serena.farrelly@gmail.com>
2024-06-26 11:04:29 +0800

    Making da Big Bois

--- LargeHEA/Gen_Large_Lattice.sh
+++ LargeHEA/Gen_Large_Lattice.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-../atomsk_b0.13.1_Linux-amd64/atomsk --create bcc 2.866 Ni Ti orient [100] [010] [001] -duplicate 24 24 24 pos
+../atomsk_b0.13.1_Linux-amd64/atomsk --create bcc 3.128 Ni Ti orient [100] [010] [001] -duplicate 18 18 18 pos
