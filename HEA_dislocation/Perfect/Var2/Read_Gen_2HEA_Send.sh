3a8270eca8c784dd9483e6b92639af481b7956de
Sez26 <serena.farrelly@gmail.com>
2024-07-01 15:10:29 +0800

    File arranging and doing dislocation tings with SNAP var 2

---
+++ HEA_dislocation/Perfect/Var2/Read_Gen_2HEA_Send.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+../../../atomsk_b0.13.1_Linux-amd64/atomsk --create bcc 3.128 Ni Ti orient [100] [010] [001] -duplicate 18 18 18 pos
+
+python intoHEA_4.py
+
+scp HEA_II.lmp sfarrelly2024@slurm-ui04.twgrid.org:/ceph/work/CNNL/sfarrelly2024/HEA_dislocation/Perfect/Var2
